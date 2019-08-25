7088243b0720f4da258856ae40aee617c56e9e5d
Martin Dimitrov <mdimitrov01@gmail.com>
2019-08-25 12:51:10 +0100

    Added upload to bucket view

--- AcoSim/data_manager/urls.py
+++ AcoSim/data_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from data_manager.views import CreateBucket, CheckBucket
+from data_manager.views import CreateBucket, CheckBucket, UploadToBucket
 
 urlpatterns = [
     path('create_bucket/', CreateBucket.as_view()),
     path('check_bucket/', CheckBucket.as_view()),
+    path('upload_to_bucket/', UploadToBucket.as_view()),
 ]
--- AcoSim/data_manager/views.py
+++ AcoSim/data_manager/views.py
@@ -27,3 +27,18 @@ class CheckBucket(View):
             }
         )
         return HttpResponse(req.text)
+
+class UploadToBucket(View):
+  def get(self, request):
+        req = requests.put(
+            'https://developer.api.autodesk.com/oss/v2/buckets/acosimbucket/objects/room.3ds',
+            headers = {
+                'Content-Type': 'application/octet-stream',
+                'Content-Length': '308331',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJjbGllbnRfaWQiOiJTbkg0MUwzdjFPcWRlRFpHSUlaQWY3b0V6MFdYcVZDeiIsImV4cCI6MTU2NjczNDQ1Mywic2NvcGUiOlsiYnVja2V0OmNyZWF0ZSIsImJ1Y2tldDpyZWFkIiwiZGF0YTp3cml0ZSJdLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbS9hdWQvand0ZXhwNjAiLCJqdGkiOiJJR3luQWE0ZlozN0h3N0dSMjNrVjJUZDNXdmhXUWxrTG5LNzFWbko0MVpoNkhpZzRVOENyRElFSzBjV2RzQmoxIn0.wW4PZvX9i-u-fpW_abYRbbbMREJ02sXUfCacb81ccMo',
+            },
+            files = {
+                'file': open('/home/martin/dev/AcoSim/AcoSim/data_manager/room.3ds', 'rb')
+            }
+        )
+        return HttpResponse(req.text)
