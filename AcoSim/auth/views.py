9b61b9d935259f6996dad26e40be818b8aa506e3
Martin Dimitrov <mdimitrov01@gmail.com>
2019-08-25 11:16:06 +0100

    Getting the token from the server

--- AcoSim/auth/views.py
+++ AcoSim/auth/views.py
@@ -1,8 +1,21 @@
+import requests
+
 from django.http import HttpResponse
 from django.views import View
 
 
 class GetAuthToken(View):
     def get(self, request):
-        # <view logic>
-        return HttpResponse('token received')
+        req = requests.post(
+        	'https://developer.api.autodesk.com/authentication/v1/authenticate',
+        	headers = {
+        		'Content-Type': 'application/x-www-form-urlencoded',
+        	},
+        	data = {
+        		'client_id': 'SnH41L3v1OqdeDZGIIZAf7oEz0WXqVCz',
+        		'client_secret': 'AjAqZKwaPLVD8C9e',
+        		'grant_type': 'client_credentials',
+        		'scope': 'data:read',
+        	}
+        )
+        return HttpResponse(req.text)
